f341487acce5205ffc260bba502623cc1fb5210d
Roberto Moreno <rampmaster@gmail.com>
2012-12-02 17:42:57 -0300

    Guardando cambio

--- application/views/ficha.php
+++ application/views/ficha.php
@@ -8,18 +8,270 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-	<title>FriendlyFood</title>
+	<title>Pollos en Dieta</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="Ficha de calidad de un alimento">
 	<meta name="author" content="Chubasco">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+		
 	<link rel="shortcut icon" href="/favicon.ico">
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-	<link rel="stylesheet" href="/assets/css/style.css?v=2">
+	<link rel="stylesheet" href="css/style.css?v=2">
+ 
+ <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet" media="screen">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <style>
 
-	<script src="/assets/js/libs/modernizr-2.0.6.min.js"></script>
-    <script src="/assets/js/jquery.js"></script>
+    /* GLOBAL STYLES
+ /* GLOBAL STYLES
+    -------------------------------------------------- */
+    /* Padding below the footer and lighter body text */
+
+    body {
+      padding-bottom: 40px;
+      color: #5a5a5a;
+    }
+
+
+
+    /* CUSTOMIZE THE NAVBAR
+    -------------------------------------------------- */
+
+    /* Special class on .container surrounding .navbar, used for positioning it into place. */
+    .navbar-wrapper {
+      position: relative;
+      z-index: 10;
+      margin-top: 20px;
+      margin-bottom: -90px; /* Negative margin to pull up carousel. 90px is roughly margins and height of navbar. */
+    }
+
+    /* Remove border and change up box shadow for more contrast */
+    .navbar .navbar-inner {
+      border: 0;
+      -webkit-box-shadow: 0 2px 10px rgba(0,0,0,.25);
+         -moz-box-shadow: 0 2px 10px rgba(0,0,0,.25);
+              box-shadow: 0 2px 10px rgba(0,0,0,.25);
+    }
+
+    /* Downsize the brand/project name a bit */
+    .navbar .brand {
+      padding: 14px 20px 16px; /* Increase vertical padding to match navbar links */
+      font-size: 16px;
+      font-weight: bold;
+      text-shadow: 0 -1px 0 rgba(0,0,0,.5);
+    }
+
+    /* Navbar links: increase padding for taller navbar */
+    .navbar .nav > li > a {
+      padding: 15px 20px;
+    }
+
+    /* Offset the responsive button for proper vertical alignment */
+    .navbar .btn-navbar {
+      margin-top: 10px;
+    }
+
+
+
+    /* CUSTOMIZE THE NAVBAR
+    -------------------------------------------------- */
+
+    /* Carousel base class */
+    .carousel {
+      margin-bottom: 60px;
+    }
+
+    .carousel .container {
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
+
+    .carousel-control {
+      background-color: transparent;
+      border: 0;
+      font-size: 120px;
+      margin-top: 0;
+      text-shadow: 0 1px 1px rgba(0,0,0,.4);
+    }
+
+    .carousel .item {
+      height: 500px;
+    }
+    .carousel img {
+      min-width: 100%;
+      height: 500px;
+    }
+
+    .carousel-caption {
+      background-color: transparent;
+      position: static;
+      max-width: 550px;
+      padding: 0 20px;
+      margin-bottom: 100px;
+    }
+    .carousel-caption h1,
+    .carousel-caption .lead {
+      margin: 0;
+      line-height: 1.25;
+      color: #fff;
+      text-shadow: 0 1px 1px rgba(0,0,0,.4);
+    }
+    .carousel-caption .btn {
+      margin-top: 10px;
+    }
+
+
+
+    /* MARKETING CONTENT
+    -------------------------------------------------- */
+
+    /* Center align the text within the three columns below the carousel */
+    .marketing .span4 {
+      text-align: center;
+    }
+    .marketing h2 {
+      font-weight: normal;
+    }
+    .marketing .span4 p {
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+
+
+    /* Featurettes
+    ------------------------- */
+
+    .featurette-divider {
+      margin: 80px 0; /* Space out the Bootstrap <hr> more */
+    }
+    .featurette {
+      padding-top: 120px; /* Vertically center images part 1: add padding above and below text. */
+      overflow: hidden; /* Vertically center images part 2: clear their floats. */
+    }
+    .featurette-image {
+      margin-top: -120px; /* Vertically center images part 3: negative margin up the image the same amount of the padding to center it. */
+    }
+
+    /* Give some space on the sides of the floated elements so text doesn't run right into it. */
+    .featurette-image.pull-left {
+      margin-right: 40px;
+    }
+    .featurette-image.pull-right {
+      margin-left: 40px;
+    }
+
+    /* Thin out the marketing headings */
+    .featurette-heading {
+      font-size: 50px;
+      font-weight: 300;
+      line-height: 1;
+      letter-spacing: -1px;
+    }
+
+
+
+    /* RESPONSIVE CSS
+    -------------------------------------------------- */
+
+    @media (max-width: 979px) {
+
+      .container.navbar-wrapper {
+        margin-bottom: 0;
+        width: auto;
+      }
+      .navbar-inner {
+        border-radius: 0;
+        margin: -20px 0;
+      }
+
+      .carousel .item {
+        height: 500px;
+      }
+      .carousel img {
+        width: auto;
+        height: 500px;
+      }
+
+      .featurette {
+        height: auto;
+        padding: 0;
+      }
+      .featurette-image.pull-left,
+      .featurette-image.pull-right {
+        display: block;
+        float: none;
+        max-width: 40%;
+        margin: 0 auto 20px;
+      }
+    }
+
+
+    @media (max-width: 767px) {
+
+      .navbar-inner {
+        margin: -20px;
+      }
+
+      .carousel {
+        margin-left: -20px;
+        margin-right: -20px;
+      }
+      .carousel .container {
+
+      }
+      .carousel .item {
+        height: 300px;
+      }
+      .carousel img {
+        height: 300px;
+      }
+      .carousel-caption {
+        width: 65%;
+        padding: 0 70px;
+        margin-bottom: 40px;
+      }
+      .carousel-caption h1 {
+        font-size: 30px;
+      }
+      .carousel-caption .lead,
+      .carousel-caption .btn {
+        font-size: 18px;
+      }
+
+      .marketing .span4 + .span4 {
+        margin-top: 40px;
+      }
+
+      .featurette-heading {
+        font-size: 30px;
+      }
+      .featurette .lead {
+        font-size: 18px;
+        line-height: 1.5;
+      }
+
+    }
+    </style>
+
+	 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+ 
+	<script src="assets/js/libs/modernizr-1.7.min.js"></script>
+    <script src="assets/js/jquery.js"></script>
 
 	 <script type="text/javascript">	
 	 
@@ -34,21 +286,46 @@
 				return results[1];
 		}	 
 		
+		function semaforo(label,id)
+		{
+			if(id==0)
+			{
+				$(label).append('<em> producto no recomendado </em>');
+				$(label).append('<img src="assets/img/green-signal.png" alt="Rojo"/>');
+				$(label).append('assets/img/green-signal.png');
+			}
+			else if(id==1)
+			{
+				$(label).append('<em> precaucion </em>');
+				$(label).append('<img src="assets/img/yellow-signal.png" alt="Amarillo"/>');
+			}
+			else if(id==2)
+			{
+				$(label).append('<em> producto no recomendado </em>');
+				$(label).append('<img src="assets/img/red-signal.png" alt="Rojo"/>');
+			}
+			else
+			{
+				$(label).append('error');
+			}
+		}
+		
 		$(document).ready(function () {
-			//var frank_param = gup('codigo');
-			//alert(frank_param);
+			// var frank_param = "8480000107480"; // gup( 'code' );
+			var frank_param = <?=$search?>;
+			// alert(frank_param);
 			$.ajax({ // ajax call starts
-				url: 'http://api.friendlyfood.me/api/search/producto/code/<?=$search?>/format/json', 
+				url: 'http://dal.friendlyfood.me/api/search/producto/code/'+frank_param+'/format/json', 
 				dataType: 'json', // Choosing a JSON datatype
 				success: 
 				function(data) // Variable data constains the data we get from serverside
 				{
-					$('#producto').append(data['NombreProducto'] + '<br/>');
-					$('#calorias').append(data['KcalFlag'] + '<br/>');
-					$('#grasas').append(data['GrasaFlag'] + '<br/>');
-					$('#saturadas').append(data['GrasaSaturadaFlag'] + '<br/>');
-					$('#azucares').append(data['HdeCFlag'] + '<br/>');
-					$('#sodio').append(data['SodioFlag'] + '<br/>');
+					$('#producto').append(data['NombreProducto'] + '');					
+					semaforo('#calorias',data['KcalFlag']);
+					semaforo('#grasas',data['GrasaFlag']);
+					semaforo('#saturadas',data['GrasaSaturadaFlag']);
+					semaforo('#azucares',data['HdeCFlag']);					
+					semaforo('#sodio',data['SodioFlag']);					
 				}
 			});
 		});	        
@@ -57,30 +334,81 @@
 </head>
 
 <body>
-	<div id="container">
-        <div id="contact-form" class="clearfix">
-			<form method="post" action="">
-		        <div id="labelProducto">Producto</div>
-		        <div id="producto"></div>
-				
-		        <div id="labelComponente">Calorias</div>
-				<div id="calorias"></div>
-				
-		        <div id="labelComponente">Grasas</div>
-				<div id="grasas"></div>
-				
-		        <div id="labelComponente">Grasas Saturadas</div>
-				<div id="saturadas"></div>
-				
-		        <div id="labelComponente">azucares</div>
-				<div id="azucares"></div>
-				
-		        <div id="labelComponente">sodio</div>
-				<div id="sodio"></div>
-				<a href="/" class="btn" type="submit">Volver</button>
-			</form>
-		</div>
-	</div>
+
+<!-- NAVBAR
+    ================================================== -->
+    <!-- Wrap the .navbar in .container to center it on the page and provide easy way to target it with .navbar-wrapper. -->
+    <div class="container navbar-wrapper">
+
+      <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+          <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">Project name</a>
+          <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="./index.html">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!-- /.navbar-inner -->
+      </div><!-- /.navbar -->
+
+    </div><!-- /.container -->
+
+      <!-- START THE FEATURETTES -->
+      <div class="featurette">	  
+		  <hr class="featurette-divider">
+		  <div class="wrapper-ficha">
+		  <p class="detail-title">Producto</p>
+		  <div id="producto"></div>
+		  <div class="wrapper-sugerencia">
+		  <p class="detail-title">Sugerencia</p>
+		  <p class="sugerencia-info">
+		  Este producto se debe consumir con precaucon
+		  </p>	  
+		  </div>
+		  
+		  <ul class="list-results">
+			<li class="red-class"> <strong>Calorias</strong>
+			<div id="calorias"></div>
+			
+			<li class="green-class"> <strong>Grasas</strong> 
+			<div id="grasas"></div>
+
+			<li class="yellow-class"> <strong>Grasas Saturadas</strong> 
+			<div id="saturadas"></div>
+			
+			<li class="red-class"> <strong>Sodio</strong> 
+			<div id="sodio"></div>
+			
+			<li class="green-class"> <strong>Azucares</strong> 
+			<div id="azucares"></div>
+		  </ul>
+		  </div>
+		  <button value="volver" type="submit">Volver</button>
+      </div>
+
+      <!-- /END THE FEATURETTES -->
 
 	
 </body>
